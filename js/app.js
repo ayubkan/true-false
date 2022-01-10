98019b27b79cdfb5a82e1fab94ab619da00fe399
ayubkan <95558567+ayubkan@users.noreply.github.com>
2022-01-10 10:22:37 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,49 @@
+function nargiza() {
+    document.querySelector('.mario_nargiza').style.backgroundColor = "green"
+    document.querySelector('.fuu').innerHTML = "Вы успешно добавлены";
+}
+
+function program() {
+    document.querySelector('.mario_programmer').style.backgroundColor = "red"
+    document.querySelector('.truu').innerHTML = "Вы вышли"; 
+}
+
+function roma() {
+    document.querySelector('.mario_roma').style.backgroundColor = "green"
+    document.querySelector('.roma').innerHTML = "Вы успешно добавлены"; 
+}
+
+function zebo() {
+    document.querySelector('.mario_zebo').style.backgroundColor = "red"
+    document.querySelector('.zebo').innerHTML = "Вы вышли"; 
+}
+
+function abror() {
+    document.querySelector('.mario_abror').style.backgroundColor = "green"
+    document.querySelector('.abror').innerHTML = "Вы успешно добавлены"; 
+}
+
+function toxir() {
+    document.querySelector('.mario_toxir').style.backgroundColor = "red"
+    document.querySelector('.toxir').innerHTML = "Вы вышли"; 
+}
+
+
+
+
+
+// let a = document.querySelector('.mario_nargiza')
+
+
+
+// let b = document.querySelector('.fuu')
+
+
+// function nargiza() {    
+//     a.classList.toggle('active')
+//     document.querySelector('.fuu').innerHTML = "Вы успешно добавлены";
+// }
+
+
+
+
